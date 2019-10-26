daa8c42dce6b7d6e9f44a4e7418a193ff470a8a5
Aditya Tapshalkar <aditya.taps@gatech.edu>
2019-10-26 17:20:29 -0400

    beautification mk 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Tesseract from "tesseract.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    imageSrc: null,
+    crop: { x: 0, y: 0 },
+    zoom: 1,
+    aspect: 4 / 3,
+  };
+
+  onFileChange = async e => {
+    if (e.target.files && e.target.files.length > 0) {
+      const imageDataUrl = await readFile(e.target.files[0]);
+      Tesseract.recognize(
+          imageDataUrl,
+          'eng',
+          { logger: m => console.log(m) }
+      ).then(({ data: { text } }) => {
+        console.log(text);
+      })
+    }
+  };
+
+  render() {
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo"/>
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+
+          <div className="App">
+            <input type="file" onChange={this.onFileChange} />
+          </div>
+
+
+        </div>
+    );
+  }
+}
+
+function readFile(file) {
+
+  return new Promise(resolve => {
+    const reader = new FileReader()
+    reader.addEventListener('load', () => resolve(reader.result), false);
+    reader.readAsDataURL(file)
+  })
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,61 +1,62 @@
 import React, { Fragment } from 'react'
 import ReactDOM from 'react-dom'
 import Tesseract from 'tesseract.js';
+import App from './App';
 
 // import Slider from '@material-ui/lab/Slider'
 // import Cropper from 'react-easy-crop'
 import './styles.css'
 
-class App extends React.Component {
-  state = {
-    imageSrc: null,
-    crop: { x: 0, y: 0 },
-    zoom: 1,
-    aspect: 4 / 3,
-  }
+// class App2 extends React.Component {
+//   state = {
+//     imageSrc: null,
+//     crop: { x: 0, y: 0 },
+//     zoom: 1,
+//     aspect: 4 / 3,
+//   }
+//
+//   // onCropChange = crop => {
+//   //   this.setState({ crop })
+//   // }
+//
+//   // onCropComplete = (croppedArea, croppedAreaPixels) => {
+//   //   console.log(croppedArea, croppedAreaPixels)
+//   // }
+//
+//   // onZoomChange = zoom => {
+//   //   this.setState({ zoom })
+//   // }
+//
+//   onFileChange = async e => {
+//     if (e.target.files && e.target.files.length > 0) {
+//       const imageDataUrl = await readFile(e.target.files[0]);
+// 	Tesseract.recognize(
+// 		  imageDataUrl,
+// 		  'eng',
+// 		  { logger: m => console.log(m) }
+// 		).then(({ data: { text } }) => {
+// 		  console.log(text);
+// 		})
+// 		    }
+//   }
+//
+//   render() {
+//     return (
+//       <div className="App">
+//         <input type="file" onChange={this.onFileChange} />
+//       </div>
+//       )
+//   }
+// }
+//
+// function readFile(file) {
+//
+//   return new Promise(resolve => {
+//     const reader = new FileReader()
+//     reader.addEventListener('load', () => resolve(reader.result), false);
+//     reader.readAsDataURL(file)
+//   })
+// }
 
-  // onCropChange = crop => {
-  //   this.setState({ crop })
-  // }
-
-  // onCropComplete = (croppedArea, croppedAreaPixels) => {
-  //   console.log(croppedArea, croppedAreaPixels)
-  // }
-
-  // onZoomChange = zoom => {
-  //   this.setState({ zoom })
-  // }
-
-  onFileChange = async e => {
-    if (e.target.files && e.target.files.length > 0) {
-      const imageDataUrl = await readFile(e.target.files[0])
-	Tesseract.recognize(
-		  imageDataUrl,
-		  'eng',
-		  { logger: m => console.log(m) }
-		).then(({ data: { text } }) => {
-		  console.log(text);
-		})
-		    }
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <input type="file" onChange={this.onFileChange} />
-      </div>
-      )
-  }
-}
-
-function readFile(file) {
-
-  return new Promise(resolve => {
-    const reader = new FileReader()
-    reader.addEventListener('load', () => resolve(reader.result), false)
-    reader.readAsDataURL(file)
-  })
-}
-
-const rootElement = document.getElementById('root')
-ReactDOM.render(<App />, rootElement)
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
